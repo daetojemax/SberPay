ac0cf817e82d0513b3e3b3e9745067b2b82f3a9d
Максим Ермолаев <lolo_22@live.ru>
2024-04-11 14:58:12 +0700

    added func

--- Sources/SberPay/SberPay.swift
+++ Sources/SberPay/SberPay.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SberPaySDK
+
+func pay(with invoice: String) {
+    let request = SberPaySDK.PaymentRequest(invoiceId: invoice)
+    SberPay.pay(with: request) { _ in }
+}
