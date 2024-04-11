ea067ee42b1c0da494e1b2653b95cafaa18ed20b
Максим Ермолаев <lolo_22@live.ru>
2024-04-11 14:07:35 +0700

    init

---
+++ Sources/SberPaySDK.xcframework/ios-arm64_x86_64-simulator/SberPaySDK.framework/Headers/SberPaySDK.h
@@ -0,0 +1,14 @@
+//
+//  SberPaySDK.h
+//  SberPaySDK
+//
+//  Created by Evgeny Glushkin on 25.08.2020.
+//  Copyright © 2020 Sberbank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SberPaySDK.
+FOUNDATION_EXPORT double SberPaySDKVersionNumber;
+//! Project version string for SberPaySDK.
+FOUNDATION_EXPORT const unsigned char SberPaySDKVersionString[];
---
+++ Tests/SberPayTests/SberPayTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SberPay
+
+final class SberPayTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
